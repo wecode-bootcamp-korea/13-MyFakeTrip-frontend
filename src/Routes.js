ae477f849eda8e6a7a3a107efa73d58d2ec153b3
JIAN <jian7061@gmail.com>
2020-11-04 19:12:10 +0900

    Add: Nav 및 메인 페이지 레이아웃 완성

--- src/Routes.js
+++ src/Routes.js
@@ -11,9 +11,19 @@ import HotelDetails from './pages/HotelDetails/HotelDetails.component';
 import './style/common.scss';
 import HotelList from './pages/HotelList/HotelList.component';
 
+<<<<<<< HEAD
 import './Routes.scss';
+=======
+import Nav from "./components/Nav/Nav.component";
+import AirTickets from "./pages/AirTickets/AirTickets.component";
+import Login from "./pages/Login/Login.component";
+import Main from "./pages/Main/Main.component";
+import Purchase from "./pages/Purchase/Purchase.component";
+import SignUp from "./pages/SignUp/SignUp.component";
+>>>>>>> 0ee4015... 임시 커밋
 
 function Routes() {
+<<<<<<< HEAD
 	return (
 		<div className="Routes">
 			<Nav />
@@ -31,6 +41,23 @@ function Routes() {
 			<Footer />
 		</div>
 	);
+=======
+  return (
+    <div className="Routes">
+      <Nav />
+
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/purchase" component={Purchase} />
+          <Route exact path="/airtickets" component={AirTickets} />
+        </Switch>
+      </div>
+    </div>
+  );
+>>>>>>> 0959677... 임시 커밋
 }
 
 export default withRouter(Routes);
--- src/components/Nav/Nav.component.js
+++ src/components/Nav/Nav.component.js
@@ -1,63 +1,102 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Nav.styles.scss';
+import React, { useState, useEffect } from "react";
+
+import NavBar from "./NavBar/NavBar.component";
+import { Link, useHistory } from "react-router-dom";
+import styled from "styled-components";
+import SearchBar from "../Search/SearchBar.component";
+import List from "./List.componenet";
 
 function Nav() {
-	return (
-		<div className="Nav">
-			<nav className="nav_bar">
-				<div className="left">
-					<Link to="/">
-						<img src="data/myrealtrip_logo.png" alt="myrealtrip_logo" />
-					</Link>
-					<div className="search">
-						<input type="text" placeholder="여행지나 상품을 검색해 보세요" />
-					</div>
-				</div>
-				<ul>
-					<li>
-						<a href="#">파트너 등록하기</a>
-					</li>
-					<li>
-						<a href="#">로그인</a>
-					</li>
-					<li>
-						<a href="#">회원가입</a>
-					</li>
-				</ul>
-			</nav>
+  const [isLogin, setIsLogin] = useState(false);
+  const [isFocus, setIsFocus] = useState(false);
+  const history = useHistory();
+  const [inputValue, setInputValue] = useState("");
+  const [isClick, setIsClick] = useState(false);
+  const [isSearchActive, setIsSearchActive] = useState(true);
+  const [isHotel, setIsHotel] = useState(false);
+  // console.log(history.location.pathname, "sdfklsdlfjlksdfklsjf");
 
-			<div className="nav_list">
-				<ul>
-					<li>
-						<a href="#">홈</a>
-					</li>
-					<li>
-						<a href="#">항공권</a>
-					</li>
-					<li>
-						<a href="#">호텔</a>
-					</li>
-					<li>
-						<a href="#">펜션&캠핑</a>
-						<div className="n_circle">N</div>
-					</li>
-					<li>
-						<a href="#">투어&티켓</a>
-					</li>
-					<li>
-						<a href="#">랜선투어</a>
-					</li>
-					<li>
-						<a href="#">국내골프</a>
-					</li>
-					<li>
-						<a href="#">할인혜택</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    // console.log(history.location.pathname);
+    if (history.location.pathname !== "/") {
+      setIsSearchActive(false);
+    }
+  }, [history.location.pathname]);
+
+  return (
+    <>
+      <NavBar>
+        <Header>
+          <div>
+            <img src="./data/faketrip.png" alt="myfaketrip logo" />
+            <input type="text" placeholder="여행지나 상품을 검색해보세요" />
+          </div>
+          <Linkto>
+            <Link to="#">파트너 등록하기</Link>
+            <Link to="/login">로그인</Link>
+            <Link to="/signup">회원가입</Link>
+          </Linkto>
+        </Header>
+      </NavBar>
+    </>
+  );
 }
-
+const Nav = styled.div``;
+const NavBar = styled.div`
+  height: 120px;
+  padding: 20px 110px;
+  border-bottom: 1px solid #dee2e6;
+`;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 15px;
+  img {
+    width: 150px;
+    margin-right: 30px;
+  }
+  input {
+    width: 340px;
+    height: 45px;
+    border-radius: 4px;
+    border: none;
+    padding-left: 20px;
+    color: #495056;
+    background-color: #f5f6f7;
+    :focus {
+      outline: none;
+    }
+  }
+`;
+const Linkto = styled.div`
+  margin-top: 15px;
+  a {
+    color: rgba(73, 80, 86, 0.7);
+    font-size: 15px;
+    padding: 7px 25px;
+    &:last-child {
+      border: 2px solid #a7d4f9;
+      border-radius: 3px;
+      color: #2b96ed;
+    }
+  }
+`;
+const List = styled.div`
+  span {
+    margin-right: 30px;
+    padding-bottom: 10px;
+    color: rgba(73, 80, 86, 0.7);
+    font-size: 15px;
+    position: relative;
+    :hover {
+      color: #ffffff;
+      border-bottom: 2px solid #ffffff;
+    }
+  }
+  img {
+    position: absolute;
+    top: 73px;
+    left: 340px;
+  }
+`;
 export default Nav;
