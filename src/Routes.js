c697ef269c074523c4bce2f67f53efd502234462
JIAN <jian7061@gmail.com>
2020-11-07 17:30:40 +0900

    Signup 컴포넌트 레이아웃 완성

--- src/Routes.js
+++ src/Routes.js
@@ -7,7 +7,7 @@ import AirTickets from "./pages/AirTickets/AirTickets.component";
 import Login from "./pages/Login/Login.component";
 import Main from "./pages/Main/Main.component";
 import Purchase from "./pages/Purchase/Purchase.component";
-import SignUp from "./pages/SignUp/SignUp.component";
+import Signup from "./pages/Signup/Signup.component";
 
 function Routes() {
   return (
@@ -17,7 +17,7 @@ function Routes() {
         <Switch>
           <Route exact path="/" component={Main} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/signup" component={Signup} />
           <Route exact path="/purchase" component={Purchase} />
           <Route exact path="/airtickets" component={AirTickets} />
         </Switch>
